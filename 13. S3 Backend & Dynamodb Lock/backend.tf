0ec990d6ab9e271fb777e76253b49c3ad8f412b4
dorachy <99226731+dorachy@users.noreply.github.com>
2022-08-04 19:58:18 -0400

    Update backend.tf

--- 13. S3 Backend & Dynamodb Lock/backend.tf
+++ 13. S3 Backend & Dynamodb Lock/backend.tf
@@ -1,10 +1,10 @@
 # Below is Backend block
 terraform {
   backend "s3" {
-    bucket = ""
-    key = ""
-    region = ""
-    profile = ""
-    dynamodb_table = ""
+    bucket = "code-sharing-bucket"
+    key = "Ansible-main/s3file-3.tfstate"
+    region = "us-east-1"
+    profile = "jespo"
+    dynamodb_table = "firstprojecttable"
   }
 }
